b4f313c77c1f52af34e0a567bdd6ad137564bc78
Johnson Wang <drgarbage@gmail.com>
2023-11-05 18:04:13 +0800

    add overlay by default

--- console/pages/api/ipfs/[cid].js
+++ console/pages/api/ipfs/[cid].js
@@ -1,5 +1,13 @@
 import { create } from 'kubo-rpc-client';
+const axios = require('axios');
+const sharp = require('sharp');
+
 const { IPFS_HOST } = process.env;
+
+export const imageURL2Buffer = async (url) => {
+  const response = await axios.get(url, { responseType: 'arraybuffer' });
+  return Buffer.from(response.data, 'binary');
+}
 
 export default async (req, res) => {
   const {cid} = req.query;
@@ -10,7 +18,21 @@ export default async (req, res) => {
     bytes.push(...chunk);
   }
 
+  const imageBuffer = Buffer.from(bytes);
+  const overlayBuffer = await sharp(await imageURL2Buffer('https://ai.printii.com/images/logo_overlay.png'))
+    .resize(128)
+    .extend({
+      right: 20,
+      bottom: 20,
+      extendWith: 'copy'
+    })
+    .toBuffer();
+
+  const resultBuffer = await sharp(imageBuffer)
+    .composite([{ input: overlayBuffer, gravity: 'southeast' }])
+    .toBuffer();
+    
   res.setHeader('Content-Type', 'image/png');
   res.setHeader('Cache-Control', 'public, max-age=259200');
-  res.status(200).send(Buffer.from(bytes));
+  res.status(200).send(Buffer.from(resultBuffer));
 }
